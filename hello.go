36f7af3a1746333dc0096ccb9b29fcc08b1d2ef4
Lucas Barbosa <lucas.barbosa@possible.com>
2021-04-14 15:50:14 -0300

    new languages greetings and tests added

--- hello.go
+++ hello.go
@@ -2,17 +2,39 @@ package main
 
 import "fmt"
 
+// English const to use at package level
+const English string = "English"
+// Portuguese const to use at package level
+const Portuguese string = "Portuguese"
+// Spanish const to use at package level
+const Spanish string = "Spanish"
 // EnglishGreeting const to use at package level
 const EnglishGreeting = "Hello, "
+// PortugueseGreeting const to use at package level
+const PortugueseGreeting = "Olá, "
+// SpanishGreeting const to use at package level
+const SpanishGreeting = "Hola, "
 
 // Hello returns a greeting
-func Hello(name string) string {
+func Hello(name string, language string) string {
 	if name == "" {
 		name = "World"
 	}
-	return EnglishGreeting + name
+
+	return greetingMessage(language) + name
+}
+
+func greetingMessage(language string) string{
+	switch language {
+	case Portuguese:
+		return PortugueseGreeting
+	case Spanish:
+		return SpanishGreeting
+	default:
+		return EnglishGreeting
+	}
 }
 
 func main() {
-	fmt.Println(Hello("Lucas"))
+	fmt.Println(Hello("Lucas", ""))
 }
--- hello_test.go
+++ hello_test.go
@@ -15,17 +15,30 @@ func TestHello(t *testing.T) {
 	}
 
 	t.Run("sayig hello to people", func(t *testing.T) {
-		got := Hello(name)
+		got := Hello(name, "")
 		want := EnglishGreeting + name
 
 		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "")
 		want := EnglishGreeting + "World"
 
 		assertCorrectMessage(t, got, want)
 	})
 
+	t.Run("in Portuguese", func(t *testing.T) {
+		got := Hello(name, Portuguese)
+		want := PortugueseGreeting + name
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello(name, Spanish)
+		want := SpanishGreeting + name
+
+		assertCorrectMessage(t, got, want)
+	})
 }
