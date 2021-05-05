51fd9c86c7b622f47ad88346f5293e23a29e6774
Lucas Barbosa <lucas.barbosa@possible.com>
2021-05-05 08:43:03 -0300

    refactoring and adding SumAllTails function

--- arrayNSlices/sum.go
+++ arrayNSlices/sum.go
@@ -10,3 +10,39 @@ func Sum(numbers []int) int {
 	}
 	return sum
 }
+
+// SumAll returns a sum of all integers of given arrays
+func SumAll(numbersToSum ...[]int) []int {
+	// lengthOfNumbers := len(numbersToSum)
+	// sums := make([]int, lengthOfNumbers)
+
+	// for i, numbers := range numbersToSum {
+	// 	sums[i] = Sum(numbers)
+	// }
+	// above the implementation being carefull with slices capacity
+
+	var sums []int
+
+	for _, numbers := range numbersToSum {
+		sums = append(sums, Sum(numbers))
+	}
+
+	return sums
+}
+
+// SumAllTails returns a sum of all integers of given arrays less than the head of each one
+func SumAllTails(numbersToSum ...[]int) []int {
+	var tails[][] int
+	for _, numbers := range numbersToSum {
+		var newElement []int
+
+		if len(numbers) < 1 {
+			newElement = []int {0}
+		} else {
+			newElement = numbers[1:]
+		}
+
+		tails = append(tails, newElement)
+	}
+	return SumAll(tails...)
+}
--- arrayNSlices/sum_test.go
+++ arrayNSlices/sum_test.go
@@ -1,6 +1,9 @@
 package sum
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
 
@@ -30,4 +33,70 @@ func TestSum(t *testing.T) {
 		assertCorrectMessage(t, got, want, numbers)
 	})
 
+}
+
+func TestSumAll(t *testing.T) {
+	assertCorrectMessage := func(t testing.TB, got []int, want []int, numbers ...[]int) {
+		t.Helper()
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %d want %d given, %v", got, want, numbers)
+		}
+	}
+
+	t.Run("collection of 2 arrays of 5 numbers", func(t* testing.T) {
+		numbers := []int{1, 2, 3, 4, 5}
+
+		got := SumAll(numbers, numbers)
+		want := []int {15, 15}
+
+		assertCorrectMessage(t, got, want, numbers, numbers)
+	})
+
+	t.Run("collection of any arrays numbers of 5 numbers", func(t* testing.T) {
+		numbers := []int{1, 2, 3, 4, 5}
+
+		got := SumAll(numbers, numbers, numbers, numbers, numbers, numbers)
+		want := []int {15, 15, 15, 15, 15, 15}
+
+		assertCorrectMessage(t, got, want, numbers, numbers, numbers, numbers, numbers, numbers)
+	})
+}
+
+func TestSumAllTails(t *testing.T) {
+	assertCorrectMessage := func(t testing.TB, got []int, want []int, numbers ...[]int) {
+		t.Helper()
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %d want %d given, %v", got, want, numbers)
+		}
+	}
+
+	t.Run("a array of 5 numbers", func(t* testing.T) {
+		numbers := []int{1, 2, 3, 4, 5, 6}
+
+		got := SumAllTails(numbers)
+		want := []int {20}
+
+		assertCorrectMessage(t, got, want, numbers)
+	})
+
+	t.Run("collection of any arrays numbers of 5 numbers", func(t* testing.T) {
+		numbers := []int{1, 2, 3, 4, 5}
+
+		got := SumAllTails(numbers, numbers, numbers, numbers, numbers, numbers)
+		want := []int {14, 14, 14, 14, 14, 14}
+
+		assertCorrectMessage(t, got, want, numbers, numbers, numbers, numbers, numbers, numbers)
+	})
+	
+	t.Run("collection of arrays with a empty one", func(t* testing.T) {
+		numbers := []int{1, 2, 3, 4, 5}
+		emptyArray := []int{}
+
+		got := SumAllTails(numbers, emptyArray)
+		want := []int {14, 0}
+
+		assertCorrectMessage(t, got, want, numbers, emptyArray)
+	})
 }
